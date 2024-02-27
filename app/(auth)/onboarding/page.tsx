755a23ed6dcde73fb1f67ff2abed3cc5320c2140
Luis <vsjoseluisvs@gmail.com>
2024-02-26 22:32:33 -0600

    feat: form basic onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,34 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+import { currentUser } from '@clerk/nextjs';
+
 async function Page() {
+  const user = await currentUser();
+  console.log(user);
+
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || '',
+    bio: userInfo?.bio || '',
+    image: userInfo?.image || user?.imageUrl
+  }
+
   return (
-    <main>
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
+      <p className='text-light-2 mt-3 text-base-regular'>
+        Complete your profile now to use threads
+      </p>
+
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile
+          user={userData}
+          btnTitle='Continue'
+        />
+      </section>
     </main>
   );
 }
