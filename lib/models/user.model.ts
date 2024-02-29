873b4aee22b06564345c78d6c9acdfd8e036cee7
Luis <vsjoseluisvs@gmail.com>
2024-02-28 21:29:23 -0600

    feat: mongodb connection

---
+++ lib/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thread',
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Community',
+    },
+  ],
+});
+
+/**
+ * Se hace de la siguiente manera porque la primera vez el schema no existe por lo que 
+ * se tiene que crear, y la siguintes veces llamara mongoose.models.User
+ */
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
